5f204c9f540834c085c677171541726fff5f0231
maxwell <smithjohnfamilytree@live.cn>
2020-10-16 12:21:43 +0800

    添加实验一文件夹

---
+++ 实验一/sourcecode1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+const int looptimes = 5;
+int main()
+{
+    int p = fork(); //结果为-1，则创建进程失败。结果为0则为子进程，结果>0则为父进程。结果为子进程的pid。
+    if (p == -1) exit(-1);  
+    
+    if (p == 0) //  子进程
+    {
+        for(int i=0;i<looptimes;i++)
+        {
+            printf("%s","I am child.\n");
+            sleep(1);
+        }
+        
+    }
+    else if(p > 0)  //父进程
+    {
+        for(int i=0;i<looptimes;i++)
+        {
+            printf("%s","I am parent.\n");
+            sleep(1);
+        }
+    }
+    
+    
+    return 0;
+}
